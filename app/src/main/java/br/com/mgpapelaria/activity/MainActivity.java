216111a764d45e64fee8f2d925a6b44a4c4e928f
Danilo O. Lima <danilo__oliveira@hotmail.com>
2020-08-12 14:28:51 -0400

    Correção provisória

--- app/src/main/java/br/com/mgpapelaria/activity/MainActivity.java
+++ app/src/main/java/br/com/mgpapelaria/activity/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
             editor.apply();
         });*/
         //getSupportActionBar().hide();
-        //OrderManagerSingleton.getInstance();
+        OrderManagerSingleton.getInstance();
         this.configSDK();
     }
 
