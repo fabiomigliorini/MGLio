cff998d98ff8b0c3923bd0b689403896c930276d
Danilo O. Lima <danilo__oliveira@hotmail.com>
2021-01-29 11:43:21 -0400

    Implementado login por qrcode

--- app/src/main/java/br/com/mgpapelaria/activity/LoginActivity.java
+++ app/src/main/java/br/com/mgpapelaria/activity/LoginActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.crashlytics.FirebaseCrashlytics;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import br.com.mgpapelaria.R;
 import br.com.mgpapelaria.api.ApiService;
@@ -27,6 +30,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
+    private static int LOGIN_QR_REQUEST = 1000;
     @BindView(R.id.usuario_text_field)
     TextInputLayout usuarioInputLayout;
     @BindView(R.id.usuario_edit_text)
@@ -67,6 +71,39 @@ public class LoginActivity extends AppCompatActivity {
         this.apiService = RetrofitUtil.createService(this, ApiService.class);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() != null) {
+                String token = result.getContents();
+                Log.i("TOKEN", token);
+
+                ProgressDialog mDialog = new ProgressDialog(LoginActivity.this);
+                mDialog.setMessage("Aguarde...");
+                mDialog.setCancelable(false);
+                mDialog.show();
+
+                getUserInfo(token, new UserInfoCallback() {
+                    @Override
+                    public void onResponse() {
+                        SharedPreferencesHelper.setToken(LoginActivity.this, token);
+                        mDialog.dismiss();
+                        goToMainOrPinpadActivity();
+                    }
+
+                    @Override
+                    public void onError() {
+                        mDialog.dismiss();
+                        showErrorDialog("Erro ao processar a requisição ou QRCode inválido.");
+                    }
+                });
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
     @OnClick(R.id.entrar_button)
     void login(){
         if(!this.isFormValid()){
@@ -92,19 +129,8 @@ public class LoginActivity extends AppCompatActivity {
                         getUserInfo(token, new UserInfoCallback() {
                             @Override
                             public void onResponse() {
-                                Intent intent;
-
-                                if(valor != null){
-                                    intent = new Intent(LoginActivity.this, PinpadActivity.class);
-                                    intent.putExtra(PinpadActivity.VALOR, valor);
-                                    intent.setAction("br.com.mgpapelaria.PINPAD");
-                                }else{
-                                    intent = new Intent(LoginActivity.this, MainActivity.class);
-                                }
-                                startActivity(intent);
                                 mDialog.dismiss();
-                                LoginActivity.this.finish();
-                                valor = null;
+                                goToMainOrPinpadActivity();
                             }
 
                             @Override
@@ -132,14 +158,39 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    /*@OnClick(R.id.entrar_qr_button)
+    public void loginQR(){
+        //Intent intent = new Intent(this, LoginQRActivity.class);
+        //startActivityForResult(intent, LOGIN_QR_REQUEST);
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setOrientationLocked(true);
+        integrator.setPrompt("");
+        integrator.initiateScan();
+    }*/
+
+    private void goToMainOrPinpadActivity(){
+        Intent intent;
+
+        if(valor != null){
+            intent = new Intent(LoginActivity.this, PinpadActivity.class);
+            intent.putExtra(PinpadActivity.VALOR, valor);
+            intent.setAction("br.com.mgpapelaria.PINPAD");
+        }else{
+            intent = new Intent(LoginActivity.this, MainActivity.class);
+        }
+        startActivity(intent);
+        LoginActivity.this.finish();
+        valor = null;
+    }
+
     public interface UserInfoCallback{
         void onResponse();
         void onError();
     }
 
     private void getUserInfo(String token, UserInfoCallback callback){
-        this.apiService = RetrofitUtil.createService(this, ApiService.class, token);
-        this.apiService.getUser().enqueue(new Callback<UsuarioResponse>() {
+        ApiService apiService = RetrofitUtil.createService(LoginActivity.this, ApiService.class, token);
+        apiService.getUser().enqueue(new Callback<UsuarioResponse>() {
             @Override
             public void onResponse(Call<UsuarioResponse> call, Response<UsuarioResponse> response) {
                 if(response.code() == 200){
